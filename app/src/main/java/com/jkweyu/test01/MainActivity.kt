9fe9cc6fabd8cf49ec1f7d25c803338ba5fd4816
Liminghing <im349412@gmail.com>
2025-04-05 09:55:29 +0900

    edit develop branch

--- app/src/main/java/com/jkweyu/test01/MainActivity.kt
+++ app/src/main/java/com/jkweyu/test01/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // develop branch 09:54
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
